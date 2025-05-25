275c62cb2b35ff07cabc083eebcd7b915b118bbe
vik <viksharrajesh@gmail.com>
2025-05-25 10:33:43 -0500

    fixed ctrl c handling

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@
 mod vm;
 use crate::vm::vm::*;
 use crossterm::terminal::{self, disable_raw_mode};
-use ctrlc;
-use std::{process, env};
-
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent};
+use std::{process, env, time::Duration, thread};
 
 fn setup_terminal() {
 	terminal::enable_raw_mode().expect("could not enable raw mode");
-	ctrlc::set_handler(|| {
-		disable_raw_mode().ok();
-		process::exit(130); // ctrl+C exit code
-	}).expect("could not set ctrl+c handler")
+	// create new thread for ctrlc - raw mode blocks ctrl c signals
+	thread::spawn(|| {
+		loop {
+			if poll(Duration::from_millis(16)).unwrap() {
+				if let Ok(Event::Key(KeyEvent { code: KeyCode::Char('c'), modifiers, .. })) = read() {
+                    if modifiers.contains(crossterm::event::KeyModifiers::CONTROL) {
+                        disable_raw_mode().ok();
+						println!();
+                        process::exit(130);
+					}
+				}
+			}
+		}
+	});
 }
 
 fn main() {
